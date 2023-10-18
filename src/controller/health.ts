1b1b6256780182b42408f67c09c1dcde41226432
Kylian BACHELET <kykydesavoie@hotmail.com>
2023-10-18 13:50:34 +0200

    :memo: :goal_net: :heavy_plus_sign: Add POST ping route

--- src/controller/health.ts
+++ src/controller/health.ts
@@ -1,7 +1,17 @@
 import { Route, Get } from "tsoa";
 
-@Route('/health')
+@Route('api/health')
 export default class HealthController {
+
+    private static instance: HealthController;
+    private constructor() { }
+    public static getInstance(): HealthController {
+        if (!HealthController.instance) {
+            HealthController.instance = new HealthController();
+        }
+        return HealthController.instance;
+    }
+
     @Get('/')
     public async index(): Promise<void> {
         return ;
